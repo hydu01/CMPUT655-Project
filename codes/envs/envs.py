d0f3fc0f41677df5bf6feb57eb0923992a2d0821
Haruto Tanaka <harutornof@gmail.com>
2023-11-29 10:02:16 -0700

    Added few observation wrappers and refactored

--- codes/envs/envs.py
+++ codes/envs/envs.py
@@ -1,32 +1,28 @@
 import gymnasium as gym
 from gymnasium.core import Env
 import minigrid
-from minigrid.wrappers import RGBImgObsWrapper
+from minigrid.wrappers import FullyObsWrapper
 
-from .custom_minigrid_env import CustomMinigridEnv
+from .custom_minigrid_wrappers import (
+    NormalizeReward,
+    FlattenObservation,
+    TraditionalFlattenObservation,
+    CustomMinigridEnv,
+)
 
 def make_env(
     env_name: str,
-    rgb: bool = True,
+    flat_obs: bool = True,
     normalize_reward: bool = False,
+    mx_reward: float = 1.0,
+    gamma: float = 1.0,
 ):
     base_env = gym.make(env_name)
-    if rgb:
-        base_env = RGBImgObsWrapper(base_env)
+    if flat_obs:
+        base_env = TraditionalFlattenObservation(base_env)
     if normalize_reward:
-        base_env = NormalizeReward(base_env, normalize_reward)
+        base_env = NormalizeReward(base_env, mx_reward=mx_reward, gamma=gamma)
 
     # Wrap with custom action sequences
     env = CustomMinigridEnv(base_env)
     return env
-
-
-class NormalizeReward(gym.RewardWrapper):
-    def __init__(self, env: Env, mx_reward: float = 1., gamma: float = 1.):
-        super().__init__(env)
-        
-        self.mx_reward = abs(mx_reward)
-        self.gamma = gamma
-
-    def reward(self, reward: float) -> float:
-        return reward / self.mx_reward + (self.gamma - 1.)
