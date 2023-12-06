cfa891278642eaeb8b129d3ef6e97157982a7e9e
Henry Du <du2@ualberta.ca>
2023-12-05 18:28:22 -0700

    Custom bias init

--- codes/nns/nns.py
+++ codes/nns/nns.py
@@ -15,19 +15,19 @@ class Model(nn.Module):
 
 
 class Elephant(nn.Module):
-    def __init__(self, sigma=1.0, d=2.0):
+    def __init__(self, sigma=2.0, d=20.0):
         super().__init__()
         self.sigma = sigma
         self.d = d
     
     def forward(self, x):
-        return 1.0/(1.0+torch.power(torch.abs(x/self.sigma), self.d))
+        return 1.0/(1.0+torch.pow(torch.abs(x/self.sigma), self.d))
 
 
 class MLP(nn.Module):
-    def __init__(self, dims, last_layer_bias=10.0):
+    def __init__(self, dims, last_layer_bias=None):
         super().__init__()
-        self.model = make_model(dims, last_layer_bias)
+        self.model = self.make_model(dims, last_layer_bias)
     
     def make_model(self, dims, last_layer_bias):
         layers = []
@@ -35,28 +35,37 @@ class MLP(nn.Module):
             layers.append(nn.Linear(dims[i], dims[i+1]))
             layers.append(nn.ReLU())
         layers.append(nn.Linear(dims[-2], dims[-1]))
-        with torch.no_grad():
-            layers[-1].bias.fill_(last_layer_bias)
-        return nn.Sequential(layers)
+        if last_layer_bias is not None:
+                with torch.no_grad():
+                    layers[-1].bias.fill_(last_layer_bias)
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.model(x)
 
 
 class EMLP(nn.Module):
-    def __init__(self, dims, last_layer_bias=10.0):
+    def __init__(self, dims, last_layer_bias="elephant"):
         super().__init__()
-        self.model = make_model(dims, last_layer_bias)
+        self.model = self.make_model(dims, last_layer_bias)
     
     def make_model(self, dims, last_layer_bias):
         layers = []
         for i in range(len(dims)-2):
             layers.append(nn.Linear(dims[i], dims[i+1]))
+            with torch.no_grad():
+                layers[-1].bias.uniform_(-1.0, 1.0)
             if i == len(dims) - 2:
                 layers.append(Elephant())
             else:
                 layers.append(nn.ReLU())
         layers.append(nn.Linear(dims[-2], dims[-1]))
         with torch.no_grad():
-            layers[-1].bias.fill_(last_layer_bias)
-        return nn.Sequential(layers)
+            if last_layer_bias == "elephant":
+                layers[-1].bias.uniform_(-1.0, 1.0)
+            elif last_layer_bias is not None:
+                layers[-1].bias.fill_(last_layer_bias)
+        return nn.Sequential(*layers)
     
     def forward(self, x):
         return self.model(x)
