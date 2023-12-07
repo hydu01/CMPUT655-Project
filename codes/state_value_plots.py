cab18c069e88f8d166660a24c3240701c015dd1b
Alireza Azimi <alirezaazimi@Alirezas-MacBook-Pro.local>
2023-12-07 09:45:22 -0700

    cleanup

--- codes/state_value_plots.py
+++ codes/state_value_plots.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
-# Load JSON data from a file
 if __name__ == '__main__':
     environments = ["DistShift1-v0", "Empty-Random-6x6-v0", "LavaGapS5-v0"]
     for env in environments:
@@ -15,7 +14,6 @@ if __name__ == '__main__':
                 data.append(json.load(file))
         average_over_runs = {key: sum(d.get(key,0) for d in data) / len(data) for key in data[0]}
 
-        # Extract coordinates and values
         coordinates = [eval(coord) for coord in average_over_runs.keys()]
         values = list(average_over_runs.values())
 
@@ -26,18 +24,14 @@ if __name__ == '__main__':
             max_x = 3
             max_y = 3
 
-        # Create a grid for values
         value_grid = np.zeros((max_y, max_x))
 
-        # Fill the grid with values
         for coord, value in zip(coordinates, values):
             value_grid[coord[1] - 1, coord[0] - 1] = value
 
-        # Create a heatmap for values
         plt.figure()
         plt.imshow(value_grid, cmap='viridis', interpolation='nearest')
 
-        # Add text annotations for each cell
         for i in range(max_y):
             for j in range(max_x):
                 plt.text(j, i, f'{value_grid[i, j]:.2f}', ha='center', va='center', color='orange')
