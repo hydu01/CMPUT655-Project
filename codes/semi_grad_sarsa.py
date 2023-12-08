6cbf9045bd5d3a503ec4c0b4fe9ad3f779c8c6ec
Henry <henry@Henrys-MacBook-Air.local>
2023-12-07 18:20:38 -0700

    config: use_cnn -> model_type

--- codes/semi_grad_sarsa.py
+++ codes/semi_grad_sarsa.py
@@ -15,11 +15,11 @@ from utils import seed_everything, read_config, hash_env
 
 
 class Agent():
-    def __init__(self, q_net, lr, gamma, epsilon=0, use_cnn=False):
+    def __init__(self, q_net, lr, gamma, epsilon=0, model_type="mlp"):
         self.q_net = q_net
         self.gamma = gamma
         self.epsilon = epsilon
-        self.use_cnn = use_cnn
+        self.model_type = model_type
         self.optimizer = optim.Adam(self.q_net.parameters(), lr=lr)
 
     def __call__(self, state):
@@ -34,13 +34,13 @@ class Agent():
     
     def update(self, state, action, reward, next_state, next_action, terminal):
         qs_next = self.q_net(next_state).detach()
-        if self.use_cnn:
+        if self.model_type == "cnn":
             qs_next = torch.squeeze(qs_next, dim=0)
 
         target = reward + self.gamma * qs_next[next_action] * (1-terminal)
 
         q = self.q_net(state)
-        if self.use_cnn:
+        if self.model_type == "cnn":
             q = torch.squeeze(q, dim=0)
 
         loss = 1/2 * torch.square(target - q[action])
@@ -60,8 +60,8 @@ def parse_arguments():
     return args
 
 
-def format_observation(obs, use_cnn):
-    if use_cnn:
+def format_observation(obs, model_type):
+    if model_type == "cnn":
         width = int(np.sqrt(obs.shape[0] // 3))
         obs = obs.reshape((width, width, 3)).astype(np.float32)
         mean = obs.mean(axis=(0, 1), keepdims=True)
@@ -95,17 +95,20 @@ def run_single_config(config, save_dir):
                 )
     
     # Create model instance
-    if config["use_cnn"]:
+    if config["model_type"] == "cnn":
         obs_width = env.base_env.unwrapped.width - 2
         channels = [3, 16, 32, 32]
         hidden_dims = [100, 100]
         out_dim = 4
         q_function = CNN(obs_width, channels, hidden_dims, out_dim)
+    elif config["model_type"] == "mlp":
+        dims = [env.base_env.observation_space.shape[0]//3, 100, 100, 4]
+        q_function = MLP(dims)
     else:
         dims = [env.base_env.observation_space.shape[0]//3, 100, 100, 4]
         q_function = EMLP(dims)
 
-    agent = Agent(q_function, config["lr"], config["gamma"], config["eps"], config["use_cnn"])
+    agent = Agent(q_function, config["lr"], config["gamma"], config["eps"], config["model_type"])
 
     # Training algorithm
     optimism = config["normalize_reward"]
@@ -118,12 +121,11 @@ def run_single_config(config, save_dir):
         if timestep_count == 0 and optimism:
             # Optimistically initialize the model
             bias_term = optimistic_init_mlp(env, obs, agent, 3)
-            q_function = EMLP(dims, bias_term)
-            agent = Agent(q_function, config["lr"], config["gamma"], config["eps"], config["use_cnn"])
+            q_function.set_last_layer_bias(bias_term)
             optimism = False
 
         cur_pos = env.base_env.unwrapped.agent_pos
-        obs = format_observation(obs, config["use_cnn"])
+        obs = format_observation(obs, config["model_type"])
         obs = torch.Tensor(obs).to(config["device"])
         
         # Take action based on the current q_function
@@ -135,7 +137,7 @@ def run_single_config(config, save_dir):
         while not is_done:
             # Step environment
             nxt_obs, reward, term, trunc, _ = env.step(action)
-            nxt_obs = format_observation(nxt_obs, config["use_cnn"])
+            nxt_obs = format_observation(nxt_obs, config["model_type"])
             nxt_obs = torch.Tensor(nxt_obs).to(config["device"])
 
             # Update flag for the termination
